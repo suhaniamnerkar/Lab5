82b662db426b4ed88c0f00a6467c781b7d5b5097
Suhani Amnerkar <suhaniamnerkar@gmail.com>
2025-02-20 08:32:18 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-     int n , temp;
+    int n,temp;
     printf("Enter the number of elements in the array: ");
     scanf("%d",&n);
     int arr[n+1];
@@ -8,21 +8,33 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int j;
-    for(int i=0;i<n-1;i++){
-        int min=i;
-        for(j=i+1;j<n;j++){
-            if(arr[j]<arr[min]){
+    int min,max,i;
+    for (i = 0; i < n / 2; i++) {
+    min = i;
+    max= i;
+    }
+    for (int j=i+1; j<n-i; j++) {
+            if (arr[j]<arr[min]) {
                 min=j;
             }
-        }
-        if(min!=j){
-                int temp;
-                temp=arr[min];
-                arr[min]=arr[i];
-                arr[i]=temp;
+            if (arr[j]>arr[max]) {
+                max=j;
             }
-    } 
+        }
+
+    if (min!=i){
+            temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        if (max==i) {
+            max=min;
+        }
+        if (max!=n-i-1) {
+            temp=arr[n-i-1];
+            arr[n-i-1]=arr[max];
+            arr[max]=temp;
+        }
     for(int k=0;k<n;k++){
         printf("%d, ",arr[k]);
     }
